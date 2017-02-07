0431bf659167793de53d51139304f9201c210c13
Sarjeet Singh <sarjeetsingh@maprtech.com>
2017-02-07 09:04:50 -0800

    Webserver fix for using env for cluster and mount path

--- webserver-service/run.sh
+++ webserver-service/run.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
+LOG_DIR=$MAPR_MOUNT_PATH/$MAPR_CLUSTER/$2
 
 # Create /apps/logs directory to save logs on MFS using fuse mount
-mkdir -p $2
+mkdir -p $LOG_DIR
 
-java -cp $MAPR_CLASSPATH:/usr/share/mapr-apps/webserver-service.jar com.mapr.demos.WebServer $1 $2
+java -cp $MAPR_CLASSPATH:/usr/share/mapr-apps/webserver-service.jar com.mapr.demos.WebServer $1 $LOG_DIR
 
