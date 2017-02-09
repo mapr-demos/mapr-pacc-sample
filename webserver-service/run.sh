23128f5e746857a885bda15e5ffa42d959a84c4a
Sarjeet Singh <sarjeetsingh@maprtech.com>
2017-02-09 01:57:32 -0800

    Fallback mechanism for webserver logs if no mount point available

--- webserver-service/run.sh
+++ webserver-service/run.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-LOG_DIR=$MAPR_MOUNT_PATH/$MAPR_CLUSTER/$2
+# Create /apps/logs directory to save webserver logs on MFS using fuse mount
+# If no mount path, save logs to local /tmp/logs
 
-# Create /apps/logs directory to save logs on MFS using fuse mount
-mkdir -p $LOG_DIR
+DIR=$MAPR_MOUNT_PATH/$MAPR_CLUSTER
+
+if [ -d "$DIR" ]; then
+    LOG_DIR=$DIR/apps/logs
+else
+    LOG_DIR=/tmp/logs
+fi
+
+# Create Log dir and set permission for Read/Write
+sudo mkdir -p $LOG_DIR
+sudo chmod -R 777 $LOG_DIR
 
 java -cp $MAPR_CLASSPATH:/usr/share/mapr-apps/webserver-service.jar com.mapr.demos.WebServer $1 $LOG_DIR
-
